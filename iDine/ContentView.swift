cf821beeca37a67b25dda7674e5a1a2f759d28d6
13an <banjoyatakumi@gmail.com>
2023-07-08 13:22:41 +0900

    initial commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by 13an on 2023/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by 13an on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
